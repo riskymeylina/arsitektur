6115860f47216bc0b0efeb233125d1e57d10acb7
Rasyid Prayogo <rasyidp84@gmail.com>
2025-01-02 19:09:27 +0700

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -131,28 +131,7 @@
             <div class="funfact-inner-container">
                 <div class="row mb-n8">
                     <div class="col-sm-4 col-6 mb-8">
-                        <!-- Single Funfact Start -->
-                        <div class="single-funfact" data-aos="fade-up" data-aos-delay="300">
-                            <span class="odometer" data-count-to="8000"></span>
-                            <h6 class="title">Partner <br> worldwide</h6>
-                        </div>
-                        <!-- Single Funfact End -->
-                    </div>
-                    <div class="col-sm-4 col-6 mb-8">
-                        <!-- Single Funfact Start -->
-                        <div class="single-funfact" data-aos="fade-up" data-aos-delay="400">
-                            <span class="odometer" data-count-to="1250"></span>
-                            <h4 class="title">employees and <br> staffs</h4>
-                        </div>
-                        <!-- Single Funfact End -->
-                    </div>
-                    <div class="col-sm-4 col-6 mb-8">
-                        <!-- Single Funfact Start -->
-                        <div class="single-funfact" data-aos="fade-up" data-aos-delay="500">
-                            <span class="odometer" data-count-to="904"></span>
-                            <h5 class="title">project completed <br> on 60 countries</h5>
-                        </div>
-                        <!-- Single Funfact End -->
+
                     </div>
                 </div>
             </div>
@@ -160,137 +139,9 @@
     </div>
     <!-- Funfact Section End -->
 
-    <!-- Work Section Start -->
-    <div class="section work-image-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="content">
-                        <h4 class="subtitle" data-aos="fade-up" data-aos-delay="300">how we work</h4>
-                        <h2 class="title" data-aos="fade-up" data-aos-delay="300">Easy &amp; Trusted <br> Progress</h2>
-                        <p data-aos="fade-up" data-aos-delay="300">We understand that you’re hiring us to actually listen,
-                            and more <br> importantly, understand your vision, so that your home reflects your spirit and
-                            <br> personality, not ours. Above all else, when the project is finished, <br> we want you to
-                            LOVE WHERE YOU LIVE
-                        </p>
-                        <a href="#" class="pdf-file" data-aos="fade-up" data-aos-delay="300"><span
-                                class="icon icofont-file-pdf"></span>Download Offer [.PDF]</a>
-                        <a href="https://www.youtube.com/watch?v=kxPCFljwJws" class="lightbox-image video-box clearfix"
-                            data-aos="fade-up" data-aos-delay="300">See How we work<br><span
-                                class="fa-play ion ion-ios-play"><i class="ripple "></i></span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Work Section End -->
 
-    <!-- Team Section Start -->
-    <div class="section section-padding bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
 
-                    <!-- Section Title Start -->
-                    <div class="section-title" data-aos="fade-up" data-aos-delay="300">
-                        <h2 class="title">Our Team</h2>
-                    </div>
-                    <!-- Section Title End -->
 
-                </div>
-            </div>
-            <div class="row mb-n6">
-                <div class="col-lg-4 col-md-6 col-12 mb-6" data-aos="fade-up" data-aos-delay="300">
-                    <!-- Team Block Start -->
-                    <div class="team-block">
-                        <div class="inner-box">
-
-                            <!-- Team Image Start  -->
-                            <div class="image">
-                                <a href="#"><img src="assets/images/team/team-1.jpg" alt="Team"></a>
-                                <ul class="social-icons">
-                                    <li><a href="#" class="icofont-twitter"></a></li>
-                                    <li><a href="#" class="icofont-facebook"></a></li>
-                                    <li><a href="#" class="icofont-instagram"></a></li>
-                                    <li><a href="#" class="icofont-linkedin"></a></li>
-                                </ul>
-                            </div>
-                            <!-- Team Image End -->
-
-                            <!-- Team Content Start  -->
-                            <div class="team-content">
-                                <h4 class="title"><a href="#">Edward Eric Jr</a></h4>
-                                <h5 class="subtitle">CEO Founder</h5>
-                            </div>
-                            <!-- Team Content End -->
-
-                        </div>
-                    </div>
-                    <!-- Team Block End -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12 mb-6" data-aos="fade-up" data-aos-delay="400">
-
-                    <!-- Team Block Start  -->
-                    <div class="team-block">
-                        <div class="inner-box">
-
-                            <!-- Team Image Start  -->
-                            <div class="image">
-                                <a href="#"><img src="/assets/images/team/team-2.jpg" alt="Team"></a>
-                                <ul class="social-icons">
-                                    <li><a href="#" class="icofont-twitter"></a></li>
-                                    <li><a href="#" class="icofont-facebook"></a></li>
-                                    <li><a href="#" class="icofont-instagram"></a></li>
-                                    <li><a href="#" class="icofont-linkedin"></a></li>
-                                </ul>
-                            </div>
-                            <!-- Team Image End -->
-
-                            <!-- Team Content Start  -->
-                            <div class="team-content">
-                                <h4 class="title"><a href="#">Tom Holland</a></h4>
-                                <h5 class="subtitle">Architect & Project Management</h5>
-                            </div>
-                            <!-- Team Content End -->
-
-                        </div>
-                    </div>
-                    <!-- Team Block End -->
-
-                </div>
-                <div class="col-lg-4 col-md-6 col-12 mb-6" data-aos="fade-up" data-aos-delay="500">
-
-                    <!-- Team Block Start  -->
-                    <div class="team-block">
-                        <div class="inner-box">
-                            <!-- Team Image Start -->
-                            <div class="image">
-                                <a href="#"><img src="/assets/images/team/team-3.jpg" alt="Team"></a>
-                                <ul class="social-icons">
-                                    <li><a href="#" class="icofont-twitter"></a></li>
-                                    <li><a href="#" class="icofont-facebook"></a></li>
-                                    <li><a href="#" class="icofont-instagram"></a></li>
-                                    <li><a href="#" class="icofont-linkedin"></a></li>
-                                </ul>
-                            </div>
-                            <!-- Team Image End -->
-
-                            <!-- Team Content Start  -->
-                            <div class="team-content">
-                                <h4 class="title"><a href="#">Laura Erakovic</a></h4>
-                                <h5 class="subtitle">Executive & Marketing Management</h5>
-                            </div>
-                            <!-- Team Content End -->
-
-                        </div>
-                    </div>
-                    <!-- Team Block End -->
-
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Team Section End -->
 
 
 
